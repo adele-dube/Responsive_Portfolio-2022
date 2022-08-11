b1edb57ea5283388b67498ffbb296b799e634ce5
Adele_D <elmaadeledube@gmail.com>
2022-08-11 22:32:13 +0300

    Replaced all Lorem Ipsum Text

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//Function only runs the first time a user visits the page.
+//Inspect>application>storage>local storage>delete "visited" to clear cache and run function again
+
 function openingGreeting() {
   let userName = prompt("Hi! What is your name?");
 
@@ -21,3 +24,5 @@ if (firstVisit == null) {
 }
 
 firstVisit();
+
+//Function only runs the first time a user visits the page.
